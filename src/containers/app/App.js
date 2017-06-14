d410473a086a15fe72d4affbaa5db6b303398c62
fifty bridge <cj4luv@gmail.com>
2017-06-14 18:44:45 +0900

    flat list image List

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -5,52 +5,106 @@ import {
   Text,
   FlatList,
   CameraRoll,
-  Image
+  Image,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity
 } from 'react-native';
 
-let PHOTOS_COUNT_BY_FETCH = 30;
-const cameraRollArr = [];
+let PHOTOS_COUNT_BY_FETCH = 50;
 
 class App extends Component {
 
-  componentDidMount(){
-    this.fetchPhotos();
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      lastCursor: null,
+      loadingMore: false,
+      noMore: false,
+    }
   }
 
-  fetchPhotos() {
-    const fetchParams: Object = {
-      //   first: ..., // (required) The number of photos wanted in reverse order of the photo application
-      //   after: ..., // A cursor returned from a previous call to 'getPhotos'
-      //   groupTypes: ..., // Specifies which group types to filter the results to
-      //                    // One of ['Album', 'All', 'Event', 'Faces', 'Library', 'PhotoStream', 'SavedPhotos'(default)]
-      //   groupName: ..., // Specifies filter on group names, like 'Recent Photos' or custom album titles
-      //   assetType: ... // Specifies filter on assetType
-      //                  // One of ['All', 'Videos', 'Photos'(default)]
-      //    mimeTypes: '',
+  componentWillMount(){
+    this.fetch();
+  }
+
+  fetch(){
+    console.log('fetch: ' , !this.state.loadingMore);
+    if(!this.state.loadingMore){
+      //this.setState({loadingMore: true}, ()=>{this._fetch();});
+      this._fetch();
+    }
+  }
+
+  _fetch() {
+    var fetchParams = {
       first: PHOTOS_COUNT_BY_FETCH,
+      assetType: 'Photos',
     };
-
-    CameraRoll.getPhotos(fetchParams).then((response)=>{
-      let length = Object.keys(response.edges).length;
-
-      for(let i = 0; i < length; i++) {
-        cameraRollArr.push(response.edges[i].node.image.uri);
-      }
-      }).catch((error)=>{console.log(error)
-    });
+    if (this.state.lastCursor) {
+      fetchParams.after = this.state.lastCursor;
+    }
+    CameraRoll.getPhotos(fetchParams)
+      .then((data) => this._appendImages(data), (e) => console.log(e));
   }
 
+  _appendImages(data) {
+    var assets = data.edges;
+    var newState = {
+      loadingMore: false,
+    };
+    // console.log(data.page_info);
+
+    if (!data.page_info.has_next_page) {
+      newState.noMore = true;
+    }
+    const cameraRollArr = [];
+
+    if (assets.length > 0) {
+      for(let i = 0; i < assets.length; i++) {
+        cameraRollArr.push(assets[i].node.image.uri);
+      }
+      newState.loadingMore = false;
+      newState.lastCursor = data.page_info.end_cursor;
+      newState.images = this.state.images.concat(cameraRollArr);
+    }
+    this.setState(newState);
+  }
+
+  _onEndReached() {
+    console.log('_onEndReached: ' , !this.state.noMore);
+    if (!this.state.noMore) {
+      // console.log('_onEndReached: ' +this.state.noMore);
+      this.fetch();
+    }
+  }
 
   render() {
-    console.log(cameraRollArr)
     return (
-      <FlatList
-        data={cameraRollArr}
-        renderItem={({item,index}) => {
-          // console.log(item)
-          return(<Text key={index}>{item}</Text>)
-        }}
-      />
+        <FlatList
+          data={this.state.images}
+          initialNumToRender={50}
+          numColumns={3}
+
+          onEndReached={this._onEndReached.bind(this)}
+          onEndReachedThreshold={5}
+
+          enableEmptySections={true}
+          renderItem={({item,index}) => {
+            // console.log(index)
+            return(
+              <TouchableOpacity  onPress={()=>alert(index)}>
+                <Image source={{uri:item}}
+                  style={{width: 120, height: 120, borderWidth: 1, borderColor:'red'}}>
+                  <Text style={{fontSize: 15, color:'green'}}>{index}</Text>
+                </Image>
+              </TouchableOpacity>
+            );
+          }}
+          keyExtractor={(item, index) => index}
+        />
+
     );
   }
 }
